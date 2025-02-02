ef0a091392509696934cde30b87a03dd7546d878
Ian <2005fong@gmail.com>
2025-02-01 22:44:21 -0500

    Commiting the ML model

--- backend/agency/agency.py
+++ backend/agency/agency.py
@@ -1,21 +1,19 @@
 from agency_swarm import Agency
 from trend_analyzer.trend_analyzer import TrendAnalyzer
+#from AWS_image_analyzer.AWS_image_analyzer import AWSImageCaptionAgent
 from agency_swarm.util import set_openai_key
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
-
-# Set OpenAI key for agent communication
 set_openai_key(os.getenv("OPENAI_API_KEY"))
 
 trend_analyzer = TrendAnalyzer()
+#AWSImageCapion_agent = AWSImageCaptionAgent()
 
-# Create agency with communication flows
 agency = Agency(
     [
-        trend_analyzer 
+        trend_analyzer
     ],
     shared_instructions="agency_manifesto.md",
     temperature=0.7,
---
+++ backend/src/suggestionModel/recommend_solution.py
@@ -0,0 +1,82 @@
+# import pandas
+import pandas as pd
+ 
+# scikit-learn imports
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+
+tracks = pd.read_csv('dataset.csv')
+cat_col = [col for col in tracks.columns if tracks[col].dtype == 'object']
+num_col = [col for col in tracks.columns if tracks[col].dtype != 'object']
+
+unique_tracks = tracks.drop_duplicates().reset_index(drop=True)
+duplicates = unique_tracks['track_id'].duplicated()
+
+unique_tracks = unique_tracks.drop(['track_id'], axis = 1)
+
+unique_genres = unique_tracks["track_genre"].unique()
+
+label_encoder = LabelEncoder()
+unique_tracks['track_genre'] = label_encoder.fit_transform(unique_tracks['track_genre'])
+
+
+unique_tracks["track_genre"].unique()
+non_unique_rows = unique_tracks[unique_tracks.duplicated(keep=False)]
+
+
+unique_tracks = unique_tracks.drop_duplicates(subset=['track_name'], keep='first').reset_index(drop=True)
+non_unique_rows = unique_tracks[unique_tracks.duplicated(keep=False)]
+
+unique_tracks['explicit'] = unique_tracks['explicit'].astype(int)
+
+def scale_columns():
+    '''In this function, we'll be scaling the data by standardizing it, making sure that it's a dataframe,
+    printing its head and shape, and then returning the scaled dataframe.'''
+    
+    # TODO: create a StandardScaler() object, fit the scaler to the numerical data and transform the data, 
+    # convert to dataframe
+    scaler = StandardScaler()
+
+    # fit the scaler to the data and transform it
+    scaled_data = scaler.fit_transform(unique_tracks.drop(columns=['album_name', 'track_name', 'artists']))
+    scaled_df = pd.DataFrame(scaled_data, columns=unique_tracks.columns.drop(['album_name', 'track_name', 'artists']))
+
+    scaled_df.head()
+    scaled_df.shape
+    return scaled_df
+    
+
+def setup_knn(scaled_df):
+    '''Initializes the K-Nearest Neighbors model and fits it on the given scaled dataframe.'''
+    
+    # TODO: create the model, letting k = 10 and the distance formula be 'euclidean'. Then, fit it to the data
+    knn = NearestNeighbors(n_neighbors=10, metric='euclidean')
+    knn.fit(scaled_df)
+    return knn
+
+def recommend_songs(track, knn, scaled_data):
+    '''Given an input track, checks if the track is in the dataset. If so, it retrieves the row number of its first
+    appearance and uses the K-Nearest Neighbors model to find the 10 nearest neighbors. Then, it prints the 10 tracks
+    and corresponding artists.'''
+    
+
+    if track not in unique_tracks['track_name'].values:
+        print("Track not in dataset")
+    else:
+        # TODO: find the index of the track in the dataframe, then find the indices of the 10 closest tracks
+        row_index = unique_tracks[unique_tracks['track_name'] == track].index[0]
+        unique_tracks.iloc[row_index]
+        
+        distances, indices = knn.kneighbors(scaled_data.iloc[row_index].values.reshape(1, -1))
+
+        ret_tracks = []
+
+        for i in indices:
+            ret_tracks.append((unique_tracks.iloc[i]['track_name'], unique_tracks.iloc[i]['artists']))
+        print(ret_tracks)
+
+scaled_data = scale_columns()
+knn = setup_knn(scaled_data)
+input_track = input("Enter a track: ")
+recommend_songs(input_track, knn, scaled_data)
