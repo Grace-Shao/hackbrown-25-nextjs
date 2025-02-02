6179b30b2269cb4ee887960adb1422ac920bdee2
Grace-Shao <gshao8@gatech.edu>
2025-02-02 08:24:47 -0500

    main logo and updated 2 users

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,20 +14,16 @@ export default function Home() {
         Your browser does not support the video tag.
       </video>
       <div className="flex flex-col items-center justify-center min-h-screen p-2 sm:p-10 font-[family-name:var(--font-geist-sans)]">
-        {/* <header className="mb-8">
+        <main className="flex flex-col items-center gap-8">
+        <header className="absolute top-4 mt-4">
           <Image
-            src="/your-logo.svg" // Replace with your logo path
+            src="/logo.png" // Replace with your logo path
             alt="Your Logo"
             width={180}
             height={60}
             priority
           />
-        </header> */}
-        <main className="flex flex-col items-center gap-8">
-          <h1 className="text-4xl font-bold italic absolute top-20 mt-4">In 2une!</h1>
-            {/* <p className="text-lg text-center absolute top-40 mt-4">
-            Find the perfect song for your pictures
-            </p> */}
+        </header>
             <Link href="/userTypePage">
               <button className="absolute inset-0 w-full h-full bg-transparent text-white text-4xl font-bold flex items-center justify-center"
               ></button>
