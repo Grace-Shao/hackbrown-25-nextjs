584b47423699776c6a3f14496e8b7c99fad36b02
Grace-Shao <gshao8@gatech.edu>
2025-02-02 01:35:50 -0500

    contact is choose user type

--- frontend/app/components/navbar/Navbar.tsx
+++ frontend/app/components/navbar/Navbar.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image';
 
 export default function Navbar() {
     return (
-        <nav className="bg-grey bg-opacity-70 shadow-md">
+        <nav className="bg-grey bg-opacity-70 shadow-md text-gray">
             <div className="container mx-auto px-6 py-4 flex justify-center">
                 <div className="flex items-center space-x-8">
                     {/* <Image src="/gamestock logo.png" alt="GameStock Logo" width={60} height={60} /> */}
                     <Link legacyBehavior href="/">
-                        <a className="text-lg hover:text-white hover:underline"  style={{ color: '#C9C9BD' }}>Home</a>
+                        <a className="text-lg hover:text-white hover:underline">Home</a>
                     </Link>
                     <Link legacyBehavior href="/uploadPicturePage">
-                        <a className="text-lg text-black-800 hover:text-white hover:underline"  style={{ color:'#C9C9BD'}}>Upload a Picture</a>
+                        <a className="text-lg text-black-800 hover:text-white hover:underline">Upload a Picture</a>
                     </Link>
-                    <Link legacyBehavior href="/contact">
-                    <a className="text-lg hover:underline hover:text-pink-400" style={{ color: '#C9C9BD' }}>Contact</a>
+                    <Link legacyBehavior href="/userTypePage">
+                    <a className="text-lg hover:underline hover:text-pink-400">Contact</a>
                     </Link>
                 </div>
             </div>
