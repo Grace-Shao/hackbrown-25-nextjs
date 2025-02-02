07a3b2458e919d3a52edc08b8b88500af5e80494
Grace-Shao <gshao8@gatech.edu>
2025-02-02 00:57:37 -0500

    videos for both landing page and upload pictures

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,17 @@
 "use client";
-
-import Image from "next/image";
-import Navbar from "./components/navbar/Navbar";
 import Link from "next/link";
-import background from "../public/arcadebg2.png";
-// import arcadeVideo from "../public/arcadeVideo.mp4";
-import { useState } from "react";
-
 
 export default function Home() {
   return (
     <div >
       <video
         autoPlay
-        loop
         muted
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
         <source src="/arcadeVideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* <Navbar /> */}
       <div className="flex flex-col items-center justify-center min-h-screen p-2 sm:p-10 font-[family-name:var(--font-geist-sans)]">
         {/* <header className="mb-8">
           <Image
@@ -37,10 +28,8 @@ export default function Home() {
             Find the perfect song for your pictures
             </p> */}
             <Link href="/uploadPicturePage">
-            <button className="text-white text-5xl py-6 px-12 rounded 
-              mr-2 bg-transparent border-2 border-black hover:bg-pink-500 hover:scale-105 transition-all duration-300
-              absolute bottom-40 left-1/2 transform -translate-x-1/2"
-              >Start!</button>
+              <button className="absolute inset-0 w-full h-full bg-transparent text-white text-4xl font-bold flex items-center justify-center"
+              ></button>
             </Link>
         </main>
       </div>
